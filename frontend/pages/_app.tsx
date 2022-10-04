ef7fd0931d440e75aac98fb194c2def782456bf4
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-10-04 15:48:30 +0530

    setup tailwind and daisyui

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,8 @@
 import type { AppProps } from "next/app";
-import GlobalStyles from "../styles/Global.style";
+import "./../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Next App</div>;
+  return (
+    <div>
+      <button className="btn btn-active btn-primary">Button</button>
+    </div>
+  );
 };
 
 export default Home;
