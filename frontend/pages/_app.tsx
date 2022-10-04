0bb8aec0ed78bc4a177b441879e10903c9f1a95b
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-10-04 15:19:11 +0530

    init frontend project

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import GlobalStyles from "../styles/Global";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
