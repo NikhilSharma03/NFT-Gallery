f8b60b98e2b04526617cb122c14e7ff6c1fc9bce
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-10-05 14:35:04 +0530

    added user reducer and action

--- frontend/redux/store/store.ts
+++ frontend/redux/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./../reducer/user";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
