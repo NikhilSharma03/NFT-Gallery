79c506d296b8e1945abc4e89fafc8ff9efdca06f
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-10-05 14:32:34 +0530

    init redux store

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from "next/app";
 import "./../styles/global.css";
 import Layout from "../components/Layout/Layout";
+import { Provider } from "react-redux";
+import { store } from "../redux/store/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
